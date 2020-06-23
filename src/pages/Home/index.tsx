1e1866a0d20423850c848e1ffc2e2bd9a1280edc
marcos012 <marcospaulo.ad@gmail.com>
2020-06-23 01:53:57 -0300

    Estilizando componente

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import api from '../../services/api';
@@ -21,33 +21,46 @@ const Home = () => {
 
     return (
         <View>
-            <Text>{joke.type}</Text>
-            <Text>{joke.setup}</Text>
-            <Text>{joke.punchline}</Text>
+            <Text style={styles.type}>{joke.type}</Text>
+            <Text style={styles.joke}>{joke.setup}</Text>
+            <Text style={styles.joke}>{joke.punchline}</Text>
+            <View style={styles.center}>
             <RectButton style={styles.button} onPress={generateJoke} >
                 <Text style={styles.buttonText}>Generate joke</Text>
             </RectButton>
+            </View>
         </View>)
 }
 
 const styles = StyleSheet.create({
-
+    type: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        textAlign: 'center',
+        margin: 30
+    },
+    joke: {
+        marginBottom: 14,
+        textAlign: 'center',
+    },
+    center: {
+        alignItems: 'center'
+    },
     button: {
         width: '48%',
-        backgroundColor: '#34CB79',
+        backgroundColor: '#f95738',
         borderRadius: 10,
         height: 50,
+        
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center'
     },
-
     buttonText: {
         marginLeft: 8,
         color: '#FFF',
         fontSize: 16,
     },
-
-
 });
+
 export default Home;
